09c00e9ed8e962e4475d841aa51b79eb9e1062ac
darya-markova <darya.towarddreams@gmail.com>
2019-06-21 21:09:49 +0300

    BinaryTree printMatrix() procedure

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -23,20 +23,20 @@ public class BinaryTree {
             }
         }
 
-        public boolean contains(int _value) {
-            if (value == _value) {
-                return true;
-            } else if (value < _value) {
+        public Node findNode(int searchValue) {
+            if (value == searchValue) {
+                return this;
+            } else if (searchValue < value) {
                 if (left == null) {
-                    return false;
+                    return null;
                 } else {
-                    return left.contains(value);
+                    return left.findNode(searchValue);
                 }
             } else {
                 if (right == null) {
-                    return false;
+                    return null;
                 } else {
-                    return right.contains(_value);
+                    return right.findNode(searchValue);
                 }
             }
         }
@@ -66,5 +66,52 @@ public class BinaryTree {
 
     public void sort() {
         this.top.printInOrder();
+    }
+
+    public Node findNode(int searchValue) {
+        return this.top.findNode(searchValue);
+    }
+
+    public int maxDepth(Node start) {
+        if (start == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(start.left), maxDepth(start.right)) + 1;
+    }
+
+    public void printMatrix() {
+        int depth = maxDepth(this.top);
+        int width = (int)Math.pow(2, depth) + 1;
+
+        String[][] treeMatrix = new String[depth][width];
+
+        for(int i = 0; i < depth; i++) {
+            for(int j=0; j < width; j++) {
+                treeMatrix[i][j] = "";
+            }
+        }
+
+        fullMatrixIn(this.top, treeMatrix, 0, width / 2, width / 4);
+
+        for(int i = 0; i < depth; i++) {
+            for(int j=0; j < width; j++) {
+                System.out.print(treeMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    private void fullMatrixIn(Node top, String[][] matrix, int row, int col, int delta) {
+        matrix[row][col] =  String.valueOf(top.value);
+
+        if (top.left != null) {
+            fullMatrixIn(top.left, matrix, row + 1, col - delta, delta / 2);
+        }
+
+        if (top.right != null) {
+            fullMatrixIn(top.right, matrix, row + 1, col + delta, delta / 2);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
-        tree.insert(2);
-        tree.insert(5);
-        tree.insert(1);
-        tree.insert(10);
-        tree.insert(3);
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(60);
+        tree.insert(80);
+        tree.insert(32);
+        tree.insert(65);
+        tree.insert(75);
+        tree.insert(85);
+        tree.insert(34);
+        tree.insert(36);
 
-        tree.sort();
+        // tree.sort();
+        // System.out.println(tree.maxDepth(tree.top));
+        tree.printMatrix();
     }
 }
