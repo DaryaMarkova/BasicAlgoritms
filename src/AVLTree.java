49d1d0f613d32c2c2a31b5b93423a16893e9c617
darya-markova <darya.towarddreams@gmail.com>
2020-05-10 12:22:43 +0300

    AVL Tree code refactoring

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,290 +1,53 @@
 public class AVLTree {
-    private Node root = null;
-
-    private Node leftRotate(Node root) {
-        Node newRoot = root.right;
-        root.right = root.right.left;
-        newRoot.left = root;
-        root.height = setHeight(root);
-        root.size = setSize(root);
-        newRoot.height = setHeight(newRoot);
-        newRoot.size = setSize(newRoot);
-
-        return newRoot;
-    }
-
-    private Node rightRotate(Node root) {
-        Node newRoot = root.left;
-        root.left = root.left.right;
-        newRoot.right = root;
-        root.height = setHeight(root);
-        root.size = setSize(root);
-        newRoot.height = setHeight(newRoot);
-        newRoot.size = setSize(newRoot);
-
-        return newRoot;
-    }
-
-    private int balance(Node rootLeft, Node rootRight) {
-        return height(rootLeft) - height(rootRight);
-    }
-
-    private int setHeight(Node root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int rootLeft = root.left != null ? root.left.height : 0;
-        int rootRight = root.right != null ? root.right.height : 0;
-
-        return 1 + Math.max(rootLeft, rootRight);
-    }
-
-    private int setSize(Node root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int leftSize = root.left != null ? root.left.size : 0;
-        int rightSize = root.right != null ? root.right.size : 0;
-
-        return leftSize + rightSize + 1;
-    }
-
-    int height(Node root) {
-        return (root == null) ? 0 : root.height;
-    }
-
-    public Node get(Node root, int index) {
-        int leftSize = root.left == null ? 0 : root.left.size;
-
-        if (index == leftSize + 1) {
-            return root;
-        }
-
-        if (index < leftSize + 1) {
-            return get(root.left, index);
-        } else {
-            return get(root.right, index - leftSize - 1);
-        }
-    }
-
-    public Node max(Node root) {
-        if (root.left == null && root.right == null) {
-            return root;
-        }
-
-        return  (root.right == null ) ? max(root.left) : max(root.right);
-    }
-
-    public Node insert(int data) {
-        root = insert(root, data);
-
-        return root;
-    }
-
-    public Node delete(int data) {
-        root = delete(root, data);
-        return root;
-    }
-
-    private Node delete(Node root, int data) {
-        if (root == null) {
-            return root;
-        }
-
-        if (data < root.value)
-            root.left = delete(root.left, data);
-        else if (data > root.value)
-            root.right = delete(root.right, data);
-        else {
-            // one child or no child
-            if (root.left == null || root.right == null) {
-                root = (root.left == null) ? root.right : root.left;
-            } else {
-                // both children are in place
-                Node replace = findNodeToReplace(root);
-                root.value = replace.value;
-                root.left = delete(root.left, replace.value);
-            }
-        }
-
-        if (root == null)
-            return null;
-
-        // root.height = Math.max(height(root.left), height(root.right)) + 1;
-        root.height = setHeight(root);
-
-        int balance = balance(root.left, root.right);
-
-        if (balance > 1) {
-            if (height(root.left.left) >= height(root.left.right)){
-                root = rightRotate(root);
-            } else {
-                root.left = leftRotate(root.left);
-                root = rightRotate(root);
-            }
-        } else if (balance < -1) {
-            if (height(root.right.right) >= height(root.right.left)){
-                root = leftRotate(root);
-            } else {
-                root.right = rightRotate(root.right);
-                root = leftRotate(root);
-            }
-        }
-
-        root.size = setSize(root);
-        return root;
-    }
-
-    private Node insert(Node root, int data) {
-        if (root == null) {
-            return new Node(data);
-        }
-
-        if (root.value <= data) {
-            root.right = insert(root.right,data);
-        } else {
-            root.left = insert(root.left,data);
-        }
-
-        int balance = balance(root.left, root.right);
-
-        if (balance > 1) {
-            if (height(root.left.left) >= height(root.left.right)){
-                root = rightRotate(root);
-            } else {
-                root.left = leftRotate(root.left);
-                root = rightRotate(root);
-            }
-
-        } else if (balance < -1) {
-            if (height(root.right.right) >= height(root.right.left)){
-                root = leftRotate(root);
-            } else {
-                root.right = rightRotate(root.right);
-                root = leftRotate(root);
-            }
-
-        } else {
-            root.height = setHeight(root);
-            root.size = setSize(root);
-        }
-
-        return root;
-    }
-
-    private Node findNode(Node root, int data) {
-        if (root == null) {
-            return null;
-        }
-
-        if (root.value == data) {
-            return root;
-        }
-
-        if (root.value >= data) {
-            return findNode(root.left, data);
-        } else {
-            return findNode(root.right, data);
-        }
-    }
-
-    private Node findNodeToReplace(Node current) {
-        Node next = current.left;
-
-        while (next.right != null) {
-            next = next.right;
-        }
-
-        return next;
-    }
-
     public Node merge(Node v1, Node v2) {
-       Node max = max(v1);
-       v1 = delete(v1, max.value); // delete works wrong
-       return AVLMergeWithRoot(v1, v2, max);
+        Node max = v1.max();
+        v1 = v1.delete(max.value);
+        return AVLMergeWithRoot(v1, v2, max);
     }
 
-    public Node AVLMergeWithRoot(Node v1, Node v2, Node root) {
+    private Node AVLMergeWithRoot(Node v1, Node v2, Node root) {
         if (Math.abs(v1.height - v2.height) <= 1) {
             return mergeWithRoot(v1, v2, root);
         } else if (v1.height > v2.height) {
             Node newRoot = AVLMergeWithRoot(v1.right, v2, root);
             v1.right = newRoot;
-            return getRebalanced(v1);
+            return v1.getRebalanced();
         } else {
             Node newRoot = AVLMergeWithRoot(v2.right, v1, root);
             v2.right = newRoot;
-            return getRebalanced(v2);
+            return v2.getRebalanced();
         }
-    }
-
-    private Node getRebalanced(Node v) {
-        int balance = balance(v.left, v.right);
-
-        if (balance > 1) {
-            if (height(v.left.left) >= height(v.left.right)){
-                v = rightRotate(v);
-            } else {
-                v.left = leftRotate(v.left);
-                v = rightRotate(v);
-            }
-        } else if (balance < -1) {
-            if (height(v.right.right) >= height(v.right.left)){
-                v = leftRotate(v);
-            } else {
-                v.right = rightRotate(v.right);
-                v = leftRotate(v);
-            }
-        } else {
-            v.size = setSize(v);
-            v.height = setHeight(v);
-        }
-
-
-        return v;
     }
 
     private Node mergeWithRoot(Node v1, Node v2, Node root) {
         root.left = v1;
         root.right = v2;
 
-        root.height = setHeight(root);
-        root.size = setSize(root);
+        root.height = root.getNewHeight();
+        root.size = root.getNewSize();
 
         return root;
     }
 
-    public void sort() {
-        if (root == null)
-            return;
-
-        root.printInOrder();
-    }
-
     public static void main(String args[]) {
-//        AVLTree avlTree = new AVLTree();
-//        int[] values = new int[]{2,4,7,8,10,12,14,20,40,53,61,6};
-//
-//        for (int i = 0; i < values.length; i++) {
-//            avlTree.insert(values[i]);
-//        }
-//
-//        System.out.println(avlTree.max(avlTree.root).value);
 
-        AVLTree avlTree1 = new AVLTree();
-        int[] values1 = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+        int[] values1 = new int[]{1,4,2,3,5,6,8,7,9,11,10, 15, 25, 17};
 
-        for (int i = 0; i < values1.length; i++) {
-            avlTree1.insert(values1[i]);
+        Node root1  = new Node(values1[0]);
+
+        for (int i = 1; i < values1.length; i++) {
+            root1 = root1.insert(values1[i]);
         }
 
-        AVLTree avlTree2 = new AVLTree();
-        int[] values2 = new int[]{18,19,20};
+        int[] values2 = new int[] {30, 40, 50};
+        Node root2 = new Node(values2[0]);
 
-        for (int i = 0; i < values2.length; i++) {
-            avlTree2.insert(values2[i]);
+        for (int i = 1; i < values2.length; i++) {
+            root2 = root2.insert(values2[i]);
         }
+
+        AVLTree tree = new AVLTree();
+        Node merged = tree.merge(root1, root2);
+        merged.printMatrix();
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -12,6 +12,172 @@ public class Node {
         this.size = 1;
     }
 
+    public Node insert(int data) {
+        return insert(this, data);
+    }
+
+    public Node delete(int data) {
+        return delete(this, data);
+    }
+
+    public Node get(int index) {
+        return get(this, index);
+    }
+
+    public Node max() {
+        return max(this);
+    }
+
+    private Node get(Node root, int index) {
+        int leftSize = root.left == null ? 0 : root.left.size;
+
+        if (index == leftSize + 1) {
+            return root;
+        }
+
+        if (index < leftSize + 1) {
+            return get(root.left, index);
+        } else {
+            return get(root.right, index - leftSize - 1);
+        }
+    }
+
+    private Node max(Node root) {
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+
+        return  (root.right == null ) ? max(root.left) : max(root.right);
+    }
+
+    private Node delete(Node root, int data) {
+        if (root == null) {
+            return root;
+        }
+
+        if (data < root.value)
+            root.left = delete(root.left, data);
+        else if (data > root.value)
+            root.right = delete(root.right, data);
+        else {
+            // one child or no child
+            if (root.left == null || root.right == null) {
+                root = (root.left == null) ? root.right : root.left;
+            } else {
+                // both children are in place
+                Node replace = root.findNodeToReplace();
+                root.value = replace.value;
+                root.left = delete(root.left, replace.value);
+            }
+        }
+
+        if (root == null)
+            return null;
+
+        root.height = root.getNewHeight();
+
+        int balance = root.balance();
+
+        if (balance > 1) {
+            if (height(root.left.left) >= height(root.left.right)){
+                root = root.rightRotate();
+            } else {
+                root.left = root.left.leftRotate();
+                root = root.rightRotate();
+            }
+        } else if (balance < -1) {
+            if (height(root.right.right) >= height(root.right.left)){
+                root = root.leftRotate();
+            } else {
+                root.right = root.right.rightRotate();
+                root = root.leftRotate();
+            }
+        }
+
+        root.size = root.getNewSize();
+        return root;
+    }
+
+    private Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (root.value <= data) {
+            root.right = insert(root.right, data);
+        } else {
+            root.left = insert(root.left, data);
+        }
+
+        int balance = root.balance();
+
+        if (balance > 1) {
+            if (height(root.left.left) >= height(root.left.right)){
+                root = root.rightRotate();
+            } else {
+                root.left = root.left.leftRotate();
+                root = root.rightRotate();
+            }
+
+        } else if (balance < -1) {
+            if (height(root.right.right) >= height(root.right.left)){
+                root = root.leftRotate();
+            } else {
+                root.right = root.right.rightRotate();
+                root = root.leftRotate();
+            }
+
+        } else {
+            root.height = root.getNewHeight();
+            root.size = root.getNewSize();
+        }
+
+        return root;
+    }
+
+    public Node getRebalanced() {
+        int balance = balance();
+
+        if (balance > 1) {
+            if (height(this.left.left) >= height(this.left.right)){
+                return rightRotate();
+            } else {
+                this.left = this.left.leftRotate();
+                return this.left.rightRotate();
+            }
+        } else if (balance < -1) {
+            if (height(this.right.right) >= height(this.right.left)){
+                return leftRotate();
+            } else {
+                this.right = this.right.rightRotate();
+                return this.right.leftRotate();
+            }
+        } else {
+            this.size = this.getNewSize();
+            this.height = this.getNewHeight();
+        }
+
+        return this;
+    }
+
+    public void sort() {
+        this.printInOrder();
+    }
+
+    private int height(Node root) {
+        return (root == null) ? 0 : root.height;
+    }
+
+    private Node findNodeToReplace() {
+        Node next = this.left;
+
+        while (next.right != null) {
+            next = next.right;
+        }
+
+        return next;
+    }
+
     public void printInOrder() {
         if (left != null) {
             left.printInOrder();
@@ -24,12 +190,60 @@ public class Node {
         }
     }
 
+    public int balance() {
+        int leftHeight = this.left == null ? 0 : this.left.height;
+        int rightHeight = this.right == null ? 0 : this.right.height;
+
+        return leftHeight - rightHeight;
+    }
+
     public int maxDepth(Node start) {
         if (start == null) {
             return 0;
         }
 
         return Math.max(maxDepth(start.left), maxDepth(start.right)) + 1;
+    }
+
+    protected Node leftRotate() {
+        Node newRoot = this.right;
+        this.right = this.right.left;
+        newRoot.left = this;
+
+        this.height = getNewHeight();
+        this.size = getNewSize();
+
+        newRoot.height = newRoot.getNewHeight();
+        newRoot.size = newRoot.getNewSize();
+
+        return newRoot;
+    }
+
+    protected Node rightRotate() {
+        Node newRoot = this.left;
+        this.left = this.left.right;
+        newRoot.right = this;
+
+        this.height = getNewHeight();
+        this.size = getNewSize();
+        newRoot.height = newRoot.getNewHeight();
+        newRoot.size = newRoot.getNewSize();
+
+        return newRoot;
+    }
+
+    protected int getNewHeight() {
+        int rootLeft = this.left != null ? this.left.height : 0;
+        int rootRight = this.right != null ? this.right.height : 0;
+
+        return 1 + Math.max(rootLeft, rootRight);
+    }
+
+    protected int getNewSize() {
+        int leftSize = this.left != null ? this.left.size : 0;
+        int rightSize = this.right != null ? this.right.size : 0;
+
+        return leftSize + rightSize + 1;
     }
 
     public void printMatrix() {
